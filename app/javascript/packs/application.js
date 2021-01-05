e196816bca5785572b2dc9b19e6c84279fc1b338
Jared White <jared@jaredwhite.com>
2021-01-04 19:30:17 -0800

    Add custom Posture logotype to navbar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,8 +16,8 @@ require("channels")
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
+const images = require.context("../images", true)
+const imagePath = (name) => images(name, true)
 
 import "controllers"
 import "../components"
@@ -32,12 +32,12 @@ document.addEventListener("DOMContentLoaded", async () => {
   addEventListener("turbo:click", ({ target }) => {
     if (target.hasAttribute("data-turbo-preserve-scroll")) {
       scrollTop = document.scrollingElement.scrollTop
-    } 
+    }
   })
-  
+
   addEventListener("turbo:load", () => {
     if (scrollTop) {
-      document.scrollingElement.scrollTo(0, scrollTop) 
+      document.scrollingElement.scrollTo(0, scrollTop)
     }
     scrollTop = 0
   })
