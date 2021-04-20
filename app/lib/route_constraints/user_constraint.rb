9eeabc2835d02cbd07fd42ce29f9245687a5609c
Jared White <jared@jaredwhite.com>
2021-04-20 14:24:48 -0700

    Remove old route contraint files

--- app/lib/route_constraints/user_constraint.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module RouteConstraints
-  class UserConstraint
-    def current_user(request)
-      User.find_by_id(request.session[:user_id])
-    end
-  end
-end
