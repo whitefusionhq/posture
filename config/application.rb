6e2bbeb396db7b91fd000548fdc9a0cebb74aa49
Jared White <jared@jaredwhite.com>
2021-06-19 09:55:40 -0700

    Duh, actually use Sidekiq. I'm such an idiot.

--- config/application.rb
+++ config/application.rb
@@ -33,6 +33,8 @@ module Posture
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
 
+    config.active_job.queue_adapter = :sidekiq
+
     config.action_view.form_with_generates_remote_forms = false
   end
 end
